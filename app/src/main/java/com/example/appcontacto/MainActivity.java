babf982e1a3f7608ef008200e6878c80a75d8051
Erick <erick.calderon@educa.madrid.org>
2022-05-16 11:50:33 +0200

    permisos bluetooth

--- app/src/main/java/com/example/appcontacto/MainActivity.java
+++ app/src/main/java/com/example/appcontacto/MainActivity.java
@@ -68,38 +68,12 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "El Bluetooth ya esta encendido", Toast.LENGTH_SHORT).show();
                 }
             case R.id.btOff:
-                try {
-                    if (bluetoothAdapter.isEnabled()) {
-                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                            // TODO: Consider calling
-                            //    ActivityCompat#requestPermissions
-                            // here to request the missing permissions, and then overriding
-                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                            //                                          int[] grantResults)
-                            // to handle the case where the user grants the permission. See the documentation
-                            // for ActivityCompat#requestPermissions for more details.
-                            public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                            int[] grantResults) {
-                                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-                                if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
-                                    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-                                    return;
-                                }
-                                if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                                    Log.d(TAG, "Request permission success");
-                                    return;
-                                }
-                                Log.e(TAG, "Request permission failed");
-                                }
-
-                            }
-                        }
-                        bluetoothAdapter.disable();
-                    } else {
-                        Toast.makeText(this, "El Bluetooth ya esta desactivado", Toast.LENGTH_SHORT).show();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                if (bluetoothAdapter.isEnabled()) {
+                    Toast.makeText(this, "Apagando Bluetooth", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(String.valueOf(BluetoothAdapter.STATE_TURNING_OFF));
+                    someActivityResultLauncher.launch(intent);
+                } else {
+                    Toast.makeText(this, "El Bluetooth ya esta apagado", Toast.LENGTH_SHORT).show();
                 }
             case R.id.btExplorar:
             case R.id.btEmparejados:
