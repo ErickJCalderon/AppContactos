95d3f0e0be601fdcf97bc2a11b4d589ae97e5ce8
Erick <erick.calderon@educa.madrid.org>
2022-05-24 03:41:53 +0200

    Llamar Contacto

--- app/src/main/java/com/example/appcontacto/DetalleContacto.java
+++ app/src/main/java/com/example/appcontacto/DetalleContacto.java
@@ -2,12 +2,21 @@ package com.example.appcontacto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class DetalleContacto extends AppCompatActivity {
 
-    TextView tvNombre, tvApellido, tvEmail, tvTelefono;
+    TextView tvNombre, tvTelefono;
+    RelativeLayout relativeLayout;
+    String numeroContacto;
+    Button botonLlamar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +25,22 @@ public class DetalleContacto extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         String nombreContacto= bundle.getString("NOMBRECONTACTO");
-        String apellidoContacto= bundle.getString("APELLIDOCONTACTO");
-        String emailContacto= bundle.getString("EMAILCONTACTO");
-        String numeroContacto= bundle.getString("NUMERO");
+        numeroContacto= bundle.getString("NUMERO");
 
         tvNombre = findViewById(R.id.tvNombre);
-        tvApellido = findViewById(R.id.tvApellido);
-        tvEmail = findViewById(R.id.tvEmail);
         tvTelefono = findViewById(R.id.tvTelefono);
+        relativeLayout= findViewById(R.id.relativeCall);
+        botonLlamar = findViewById(R.id.botonLlamar);
 
         tvNombre.setText(nombreContacto);
-        tvApellido.setText(apellidoContacto);
-        tvEmail.setText(emailContacto);
         tvTelefono.setText(numeroContacto);
+        relativeLayout.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+numeroContacto));
+            startActivity(intent);
+        });
+    }
+    public void llamar(View v){
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+numeroContacto));
+        startActivity(intent);
     }
 }
